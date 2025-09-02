676154f18ef2fb33758492da0ed003ba2436a8fd
FIC <fic@info.local>
2025-09-01 21:51:54 -0300

    adicao ex 4, mas nao esta funcionando finalizado

--- Banco de dados/DQLEcommerce.sql
+++ Banco de dados/DQLEcommerce.sql
@@ -15,6 +15,18 @@ SELECT nome_produto
 	FROM ecommerce.produto AS p
 	JOIN ecommerce.item_pedido AS i
 	ON i.produto_id = p.produto_id
-	WHERE i.pedido_id = 1;
+WHERE i.pedido_id = 1;
 
 -- 3
+SELECT COUNT(c.cliente_id), nome_completo
+	FROM ecommerce.cliente AS c
+	JOIN ecommerce.pedido AS p
+	ON c.cliente_id = p.cliente_id
+GROUP BY c.cliente_id;
+
+-- 4
+SELECT COUNT(valor_total)
+	FROM ecommerce.pedido AS p
+	JOIN ecommerce.cliente AS c
+	ON p.cliente_id = c.cliente_id
+GROUP BY c.cliente_id;

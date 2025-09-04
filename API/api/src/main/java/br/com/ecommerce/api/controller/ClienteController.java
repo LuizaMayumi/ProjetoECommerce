fa910055139b2244444e7e0f5054876514409976
FIC <fic@info.local>
2025-09-04 20:24:33 -0300

    Criando endpoint para listar todos os usuarios

--- API/api/src/main/java/br/com/ecommerce/api/controller/ClienteController.java
+++ API/api/src/main/java/br/com/ecommerce/api/controller/ClienteController.java
@@ -1,8 +1,13 @@
 package br.com.ecommerce.api.controller;
 
+import br.com.ecommerce.api.model.Cliente;
 import br.com.ecommerce.api.service.ClienteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 // RequestMapping define a rota para encontrar a Controller cliente
@@ -11,10 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClienteController {
 //    Controller depende do Service
 //    Service depende de Repository
+//    Repository depende do Banco de dados
 
     private final ClienteService clienteService;
 
     public ClienteController(ClienteService service){
         clienteService = service;
+    }
+
+//    Listar todos
+    @GetMapping
+    public ResponseEntity<List<Cliente>> getAll(){
+        List<Cliente> clientes = clienteService.findAll();
+        return ResponseEntity.ok(clientes);
     }
 }
